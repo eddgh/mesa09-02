b3bcd82eecbc705366ab2bd0f2604a73798c1812
Edmundo Batista <eddmundosjb@gmail.com>
2023-09-15 19:03:30 -0300

    feat: projeto finalizado

--- src/components/product/index.jsx
+++ src/components/product/index.jsx
@@ -1,15 +1,24 @@
+/* eslint-disable no-self-assign */
 /* eslint-disable react/prop-types */
 
 
 import '../product/product.css'
 
-function Product({ product, rate, count}) {
+function Product({ product, rate, count }) {
 
-
-
+    //verifica se o preço tem ponto decimal, se não tiver, acrescenta .00
+    const Preco = () => {
+        var price = product.price;
+        if (price % 1 != 0 ) {
+            price = price;
+        } else { price = price + '.00'}
+        return price;
+    }
+    
     return (
         <div className="band">
             <div className='card'>
+            <p>Fakestore.api</p>
                 <div className="imgBox">
                     <img src={product.image} alt='imagem do produto ' />
                 </div>
@@ -18,15 +27,15 @@ function Product({ product, rate, count}) {
                     <h4>{product.title}</h4>
                 </div>
                 <div className="price">
-                    <h4>${product.price}</h4>
+                    <h4><span>$</span>{Preco()}</h4>
                 </div>
 
-                <div className="rate">
+                <div className="rate star">
                     <img src="./public/star-svgrepo-com.svg" alt="" />
                     <h6>{rate}</h6>
                 </div>
                 <div className="rate count">
-                    <img src="./public/emot-rate-rating-svgrepo-com.svg" alt="" />
+                    <img src="./public/emot-rate-rating-svgrepo-com.png" alt="" />
                     <h6>({count})</h6>
                 </div>
             </div>
